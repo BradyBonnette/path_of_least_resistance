d0be27f1db74c6e5d208c65aef44527d2515b1a8
Brady Bonnette <brady.bonnette@gmail.com>
2012-02-09 22:22:48 -0500

    Renamed PathOfLeastResistanceTest to something more suitable.

---
+++ src/test/java/org/example/PathOfLeastResistanceIntegrationTests.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class PathOfLeastResistanceIntegrationTests
+{
+
+}
