9aeedd8d18bbb869f65671bf060139d98f10309e
Brady Bonnette <brady.bonnette@gmail.com>
2012-02-11 22:05:38 -0500

    Removing the original PathFinder class, along with the PathFinderException class and any reference in the integration tests.

--- src/test/java/org/example/PathOfLeastResistanceIntegrationTest.java
+++ src/test/java/org/example/PathOfLeastResistanceIntegrationTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.io.*;
 import java.util.List;
 
-// Test real data using both the PathFinder and GridParser
+// Test real data using both path finders and GridParser
 public class PathOfLeastResistanceIntegrationTest
 {
     private GridParser gridParser;
@@ -50,7 +50,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
     
     @Test
-    public void test_example1_parsed_from_a_StringReader() throws GridParserException, PathFinderException {
+    public void test_example1_parsed_from_a_StringReader() throws GridParserException {
 
         gridParser.setReader(new BufferedReader(new StringReader(example1)));
         Grid grid = Grid.createGrid(gridParser);
@@ -63,7 +63,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
     
     @Test
-    public void test_example1_parsed_from_a_FileReader() throws FileNotFoundException, GridParserException, PathFinderException {
+    public void test_example1_parsed_from_a_FileReader() throws FileNotFoundException, GridParserException {
 
         gridParser.setReader(new BufferedReader(new FileReader(example1Filename)));
         Grid grid = Grid.createGrid(gridParser);
@@ -76,7 +76,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
 
     @Test
-    public void test_example2_parsed_from_a_StringReader() throws GridParserException, PathFinderException {
+    public void test_example2_parsed_from_a_StringReader() throws GridParserException {
 
         gridParser.setReader(new BufferedReader(new StringReader(example2)));
         Grid grid = Grid.createGrid(gridParser);
@@ -89,7 +89,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
 
     @Test
-    public void test_example2_parsed_from_a_FileReader() throws GridParserException, PathFinderException, FileNotFoundException {
+    public void test_example2_parsed_from_a_FileReader() throws GridParserException, FileNotFoundException {
 
         gridParser.setReader(new BufferedReader(new FileReader(example2Filename)));
         Grid grid = Grid.createGrid(gridParser);
@@ -102,7 +102,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
 
     @Test
-    public void test_example3_parsed_from_a_StringReader() throws GridParserException, PathFinderException {
+    public void test_example3_parsed_from_a_StringReader() throws GridParserException {
 
         gridParser.setReader(new BufferedReader(new StringReader(example3)));
         Grid grid = Grid.createGrid(gridParser);
@@ -115,7 +115,7 @@ public class PathOfLeastResistanceIntegrationTest
     }
 
     @Test
-    public void test_example3_parsed_from_a_FileReader() throws GridParserException, PathFinderException, FileNotFoundException {
+    public void test_example3_parsed_from_a_FileReader() throws GridParserException, FileNotFoundException {
 
         gridParser.setReader(new BufferedReader(new FileReader(example3Filename)));
         Grid grid = Grid.createGrid(gridParser);
